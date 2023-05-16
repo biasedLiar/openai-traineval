bd03cc8ae2091aa2a0baf4500f0ac0110e31c1db
biasedLiar <68741257+biasedLiar@users.noreply.github.com>
2023-05-16 20:36:52 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import json
 import openai
 
+#Replace with real key
 api_key ="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"
 openai.api_key = api_key
 
@@ -18,12 +19,13 @@ if prepare_training:
     upload_response
     print("Upload response:")
     print(upload_response)
+    file_id = upload_response["id"]
 
 else:
     #Use prepared data for training.
     file_id = "file-s615XMkyr8xNHZHI8nA53wVF" #With subjects
     #file_id = "file-vj5Wq7YSdnfQCf5LUWoyZ5wA" #Without subjects
-    
-    #Choose model to train
-    fine_tune_response = openai.FineTune.create(training_file=file_id, model="ada")
-    print(fine_tune_response)
+
+#Choose model to train
+fine_tune_response = openai.FineTune.create(training_file=file_id, model="ada")
+print(fine_tune_response)
